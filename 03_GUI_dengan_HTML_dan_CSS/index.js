776273012ac50bea71961dce38a70fc8203fc022
raditt11 <raditanurainii@gmail.com>
2026-03-09 01:33:10 +0700

    add : menambahkan TM modul 3

---
+++ 03_GUI_dengan_HTML_dan_CSS/index.js
@@ -0,0 +1,61 @@
+const editor = document.getElementById("editor-kecil");
+
+const jumlahHuruf = document.getElementById("hf");
+const hurufBesar = document.getElementById("hb");
+const hurufKecil = document.getElementById("hk");
+
+const btnBesar = document.getElementById("huruf-besar");
+const btnKecil = document.getElementById("huruf-kecil");
+const btnParagraf = document.getElementById("huruf-paragraf");
+
+
+function hitungHuruf() {
+
+    const teks = editor.value;
+
+    jumlahHuruf.textContent = teks.length;
+
+    let besar = 0;
+    let kecil = 0;
+
+    for (let huruf of teks) {
+        if (huruf >= "A" && huruf <= "Z") {
+            besar++;
+        } else if (huruf >= "a" && huruf <= "z") {
+            kecil++;
+        }
+    }
+
+    hurufBesar.textContent = besar;
+    hurufKecil.textContent = kecil;
+}
+
+
+editor.addEventListener("input", hitungHuruf);
+
+
+btnBesar.addEventListener("click", () => {
+    editor.value = editor.value.toUpperCase();
+    hitungHuruf();
+});
+
+
+btnKecil.addEventListener("click", () => {
+    editor.value = editor.value.toLowerCase();
+    hitungHuruf();
+});
+
+
+btnParagraf.addEventListener("click", () => {
+
+    const teks = editor.value.toLowerCase().split(" ");
+
+    const hasil = teks.map(kata => {
+        return kata.charAt(0).toUpperCase() + kata.slice(1);
+    });
+
+    editor.value = hasil.join(" ");
+
+    hitungHuruf();
+
+});
